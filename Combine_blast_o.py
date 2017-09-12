d8a75572b724104ee2c890637f5adb36f688c09e
Pedro J. Torres <pjtorres88@gmail.com>
2017-09-12 01:20:31 -0700

    Update Combine_blast_o.py

--- Combine_blast_o.py
+++ Combine_blast_o.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import argparse
 import os
-
+# Works as of 9-11-17
 ######################################################################################################
 ######### Split to count the number of hits given by a blast output and make an OTU style format
 ######################################################################################################
@@ -13,7 +13,7 @@ import os
 taxa={}
 # All txt files (should be blast output txt files) in current directory are added to a list 'F'
 F=[i for i in os.listdir(os.curdir) if i.split(".")[-1]=="txt"]
-
+print ("You are combining " +str(len(F))+" text Files")
 #####################################################################################################
 ####### Call and open each txt file in directory and start reading each line which should be taxonomy/gene name
 ####################################################################################################
@@ -23,12 +23,12 @@ for myfile in F:
         line=line.strip()
         line=line.split("|")# split based on this character and add them individually to a list
         taxName=line[-1]# get name of Taxa this can be changed if you want GI numbers or something else isntead
-
         """Add taxa/gene as key in dictionary and its value is a list in which each element represents taxa/gene count for each file.Checks to see if Taxa is already in the dictionary, if not, it will add it and and start to makes a list with a zero to keep count each time it sees that taxa it will add 1."""
         if taxName not in taxa:
             taxa[taxName]=[0]*len(F) # Make a list for to keep the Taxa abundace for each file in directory or length of F.
         else:pass
-        taxa[taxName][F.index(myfile)]+=1#
+        taxa[taxName][F.index(myfile)]+=1
+print "There are a total of " +str(len(taxa)) +" taxa"
 """Above it does two things. First it will be calling the values to the Key 'taxName'. But because there are a list of counters (one for each file) we need to tell it which one. By indexing all the files in 'myfile' you keep everything in order, call the right list index and add 1 each time you see that taxaName in that particular file."""
 
 #####################################################################################################
@@ -37,5 +37,7 @@ for myfile in F:
 o=open("Blast_abundance.txt","w+")
 o.write("Taxa\t"+"\t".join(F)+"\n")
 for i in taxa:
-    o.write(i+"\t"+"\t".join([str(xx) for xx in taxa[i]])+"\n")
+    o.write(i+"\t"+"\t".join([(xx) for xx in taxa[i]])+"\n")
 o.close()
+
+print "Done :)"
